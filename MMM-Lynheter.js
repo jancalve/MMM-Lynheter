7bc3cd1b3e7e706d84a6906563596ad545fbb88d
jancalve@gmail.com <jan.christian.alvestad@joint.no>
2019-11-08 02:05:42 +0100

    Chronological sorting + styling

--- MMM-Lynheter.js
+++ MMM-Lynheter.js
@@ -12,12 +12,12 @@ Module.register("MMM-Lynheter",{
 	loaded: false,
 
 	defaults: {
-		refreshInterval: 1000 * 60,
+		refreshInterval: 1000 * 60 * 2,
 		lat: 59.8260275,
 		lon: 10.8133689,
 		distance: 15,
 		httpRequestURL: 'http://139.162.242.171:8442',
-		alertDistance: 4
+		alertDistance: 5
 	},
 
 	getStyles: function () {
@@ -30,6 +30,45 @@ Module.register("MMM-Lynheter",{
 		this.loaded = false;
 		this.sendSocketNotification("CONFIG", this.config);
 	},
+
+	sortChronologically: function(news) {
+		var me = this,
+			features = news.features;	
+
+		function compare( feature1, feature2 ) {
+
+			var feature1Properties = feature1.properties;
+			var feature1Tweets = feature1Properties.tweets;
+			var feature1LatestTweet = me.minMax(feature1Tweets);
+
+			var feature2Properties = feature2.properties;
+			var feature2Tweets = feature2Properties.tweets;
+			var feature2LatestTweet = me.minMax(feature2Tweets);
+
+
+			if ( feature1LatestTweet.createdAt < feature2LatestTweet.createdAt ){
+			  return 1;
+			}
+			if ( feature1LatestTweet.createdAt > feature2LatestTweet.createdAt ){
+			  return -1;
+			}
+			return 0;
+		  }
+		  
+		  news.features = features.sort( compare );
+		  return news;
+	},
+
+	minMax: function (items) {
+		return items.reduce((acc, val) => {
+			if (acc.createdAt > val.createdAt) {
+				return acc;
+			}
+			return val;
+		}, { createdAt: 0});
+	},
+
+	
 
 	// Override dom generator.
 	getDom: function () {
@@ -72,15 +111,11 @@ Module.register("MMM-Lynheter",{
 			
 			var featureTweets = featureProperties.tweets;
 			for (var j = 0; j < featureTweets.length; j++) {
+				var currentFeatureTweet = featureTweets[j];
 				var tweetWrapper = document.createElement("div");
 				tweetWrapper.className = 'tweet';
 
-				var currentFeatureTweet = featureTweets[j];
 				var tweeter = currentFeatureTweet.tweeter;
-		//		var tweeterWrapper = document.createElement("div");
-		//		tweeterWrapper.className = 'tweeter';
-		//		tweeterWrapper.innerHTML = tweeter;
-		//		tweetWrapper.appendChild(tweeterWrapper);
 
 				var time = new Date(currentFeatureTweet.createdAt).toLocaleString("no", {  timeZone: 'UTC' });
 				var timeAndTweet = time + ' meldt av ' + tweeter;
@@ -92,7 +127,7 @@ Module.register("MMM-Lynheter",{
 				var content = currentFeatureTweet.content;
 				var contentWrapper = document.createElement("div");
 				contentWrapper.className = 'content';
-				contentWrapper.innerHTML = content;
+				contentWrapper.innerHTML = '<i>' + content + '</i>';
 				tweetWrapper.appendChild(contentWrapper);
 
 
@@ -118,11 +153,14 @@ Module.register("MMM-Lynheter",{
 			Log.log("#No data");
 			return;
 		}
+		
+		news = this.sortChronologically(news);
 
 		this.news = news;
 		this.loaded = true;
 		this.updateDom();
 	},
+
 
 	getTemplateData: function () {
 		return this.config;
